5a046d69dd59b6853f51ff48a21c8bcdaf7377ea
Samuel W Casey <scasey18@uab.edu>
2018-10-14 17:47:00 -0500

    Replace Equipment.java

--- Equipment.java
+++ Equipment.java
@@ -10,13 +10,61 @@ public class Equipment extends Item{
 	String company; //John Deere, etc.
 	String modelNumber; //MX993, A113, etc. (Sometimes model numbers have letters)
 	
-	public Equipment(String name, String equipmentType, String company, String modelNumber) {
-		super(name);
+	public Equipment(String name, float price, int locX, int locY, int length, int width,
+			String equipmentType, String company, String modelNumber) {
+		super(name,price,locX,locY,length,width);
 		this.equipmentType = equipmentType;
 		this.company = company;
 		this.modelNumber = modelNumber;
 	}
 	
+	public Equipment(String name, int locX, int locY, int length, int width,
+			String equipmentType, String company, String modelNumber) {
+		super(name,locX,locY,length,width);
+		this.equipmentType = equipmentType;
+		this.company = company;
+		this.modelNumber = modelNumber;
+	}
+	
+	public Equipment(String name, int locX, int locY, int length, int width,
+			String equipmentType, String company) {
+		super(name,locX,locY,length,width);
+		this.equipmentType = equipmentType;
+		this.company = company;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Equipment other = (Equipment) obj;
+		if (company == null) {
+			if (other.company != null)
+				return false;
+		} else if (!company.equals(other.company))
+			return false;
+		if (equipmentType == null) {
+			if (other.equipmentType != null)
+				return false;
+		} else if (!equipmentType.equals(other.equipmentType))
+			return false;
+		if (info == null) {
+			if (other.info != null)
+				return false;
+		} else if (!info.equals(other.info))
+			return false;
+		if (modelNumber == null) {
+			if (other.modelNumber != null)
+				return false;
+		} else if (!modelNumber.equals(other.modelNumber))
+			return false;
+		return true;
+	}
+
 	/**
 	 * This method adds a piece of string info to a piece of equipment
 	 * @param a, the info to be added
@@ -39,8 +87,6 @@ public class Equipment extends Item{
 	public void clearInfo() {
 		info.clear();
 	}
-	
-	
 	
 	public String getEquipmentType() {
 		return equipmentType;
