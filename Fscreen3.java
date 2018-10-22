b998ba854028db5f0856c84149518f8258839d58
Samuel W Casey <scasey18@uab.edu>
2018-10-21 21:38:25 -0500

    You can now double click to display info on a jtree item

--- Fscreen3.java
+++ Fscreen3.java
@@ -100,11 +100,22 @@ public class Fscreen3 extends JFrame{
 		addButton.addActionListener(buttonListener);
 		removeButton.addActionListener(buttonListener);
 		updateButton.addActionListener(buttonListener);
+		
 
 		tree = new JTree(rootNode);
 		tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
 		tree.setVisible(true);
-		
+		MouseListener ml = new MouseAdapter() {
+		    public void mousePressed(MouseEvent e) {
+		    	if(e.getClickCount() == 2) {
+		    		DefaultMutableTreeNode selPath = (DefaultMutableTreeNode)tree.getPathForLocation(e.getX(), e.getY()).getLastPathComponent();
+		    		showInfo(selPath.getUserObject());
+		    		
+		    	}
+		        
+		    }
+		};
+		tree.addMouseListener(ml);
 		JScrollPane scroll = new JScrollPane(tree);
 		Dimension d = scroll.getPreferredSize();
 		d.width = 800;
@@ -143,7 +154,7 @@ public class Fscreen3 extends JFrame{
             				JTextField price = new JFormattedTextField(NumberFormat.getNumberInstance());
             				Object[] message = {
             						"All of these fields are required for item creation",
-            						"Name:", name,
+            						"Name:" , name,
             						"X location:", locX,
             						"Y location:", locY,
             						"Length:", length,
@@ -381,4 +392,91 @@ public class Fscreen3 extends JFrame{
 		}
     	return null;
 	}
+	
+	public static void showInfo(Object a) {
+		
+		switch(a.getClass().toString()) {
+		case "class farming.Drone":
+			Drone drone = (Drone) a;
+			Object[] message6 = {
+					"Name: " + drone.getName(),
+					"X location: "+ drone.getLocX(),
+					"Y location: "+ drone.getLocY(),
+					"Length: "+ drone.getLength(),
+					"Width: "+ drone.getWidth(),
+					"Price: "+ drone.getPrice(),
+	    			};
+			JOptionPane.showMessageDialog(null, message6);
+			break;
+		case "class farming.Crops":
+			Crops crop = (Crops) a;
+			Object[] message5 = {
+					"Name: " + crop.getName(),
+					"X location: "+ crop.getLocX(),
+					"Y location: "+ crop.getLocY(),
+					"Length: "+ crop.getLength(),
+					"Width: "+ crop.getWidth(),
+					"Price: "+ crop.getPrice(),
+					"Crop Type: " + crop.getCropType()
+	    			};
+			JOptionPane.showMessageDialog(null, message5);
+			break;
+		case "class farming.Equipment":
+			Equipment equip = (Equipment) a;
+			Object[] message4 = {
+					"Name: " + equip.getName(),
+					"X location: "+ equip.getLocX(),
+					"Y location: "+ equip.getLocY(),
+					"Length: "+ equip.getLength(),
+					"Width: "+ equip.getWidth(),
+					"Price: "+ equip.getPrice(),
+					"Type of Equipment: " + equip.getEquipmentType(),
+	    			"Company Name: " + equip.getCompany(),
+	    			"Model Number: " + equip.getModelNumber()
+	    			};
+			JOptionPane.showMessageDialog(null, message4);
+			break;
+		case "class farming.ItemContainer":
+			ItemContainer barn = (ItemContainer) a;
+			Object[] message2 = {
+					"Name: " + barn.getName(),
+					"X location: "+ barn.getLocX(),
+					"Y location: "+ barn.getLocY(),
+					"Length: "+ barn.getLength(),
+					"Width: "+ barn.getWidth(),
+					"Price: "+ barn.getPrice()
+	    			};
+			JOptionPane.showMessageDialog(null, message2);
+			break;
+		case "class farming.LiveStock":
+			LiveStock animal = (LiveStock) a;
+			Object[] message = {
+					"Name: " + animal.getName(),
+					"X location: "+ animal.getLocX(),
+					"Y location: "+ animal.getLocY(),
+					"Length: "+ animal.getLength(),
+					"Width: "+ animal.getWidth(),
+					"Price: "+ animal.getPrice(),
+					"Type of Animal: "+ animal.getAnimalType(),
+	    			"Gender: "+ animal.getGender(),
+	    			};
+			JOptionPane.showMessageDialog(null, message);
+			break;
+		case "class farming.Supplies":
+			Supplies supply = (Supplies) a;
+			Object[] message3 = {
+					"Name: " + supply.getName(),
+					"X location: "+ supply.getLocX(),
+					"Y location: "+ supply.getLocY(),
+					"Length: "+ supply.getLength(),
+					"Width: "+ supply.getWidth(),
+					"Price: "+ supply.getPrice(),
+					"Count: " + supply.getCount()
+	    			};
+			JOptionPane.showMessageDialog(null, message3);
+			break;
+		}
+		
+	}
+	
 }
