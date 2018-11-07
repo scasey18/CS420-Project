1b94d48884058e9141e26930d228cb64d78f673a
scase <scase@LAPTOP-H5294CVK.attlocal.net>
2018-11-06 22:04:05 -0600

    Testing push

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 Download and Compile all the files located in this folder.
 Run mainFile to launch the program
+//Testing push
--- mainFile.java
+++ mainFile.java
@@ -4,6 +4,7 @@ public class mainFile {
 
     public static void main (String[] args) {
     	//Singleton a = new Singleton();
+    	//Testing push
     	Fscreen.createFscreen();
     	//Singleton is implemented because it does not create a new one
     	Fscreen.createFscreen();
