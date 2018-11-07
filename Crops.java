b73cbd17d8a4562f8ac0654a193c14e90f919582
Samuel W Casey <scasey18@uab.edu>
2018-11-06 21:40:20 -0600

    Replace Crops.java

--- Crops.java
+++ Crops.java
@@ -1,11 +1,17 @@
 package farming;
 
+import java.text.NumberFormat;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
 public class Crops extends Item{
 
 	String cropType; //The name of the type of crop 
 	//Wheat, Cotton, etc.
 	
-	public Crops(String name, float price, int locX, int locY, int length, int width,
+	public Crops(String name, int price, int locX, int locY, int length, int width,
 			String cropType,int marketValue) {
 		super(name,price,locX,locY,length,width,marketValue);
 		this.cropType = cropType;
@@ -15,7 +21,7 @@ public class Crops extends Item{
 		return name + " - Crops";
 	}
 	
-	public void updateInfo(String name, float price, int locX, int locY, int length, int width,
+	public void updateInfo(String name, int price, int locX, int locY, int length, int width,
 			String cropType,int marketValue) {
 		this.setName(name);
 		this.setPrice(price);
@@ -25,6 +31,54 @@ public class Crops extends Item{
 		this.setWidth(width);
 		this.setMarketValue(marketValue);
 		this.setCropType(cropType);
+	}
+	
+	public void showInfo() {
+		
+		JTextField name = new JTextField();
+		JTextField locX = new JFormattedTextField(NumberFormat.getNumberInstance());
+		JTextField locY = new JFormattedTextField(NumberFormat.getNumberInstance());
+		JTextField length = new JFormattedTextField(NumberFormat.getNumberInstance());
+		JTextField width = new JFormattedTextField(NumberFormat.getNumberInstance());
+		JTextField price = new JFormattedTextField(NumberFormat.getNumberInstance());
+		JTextField mPrice = new JFormattedTextField(NumberFormat.getNumberInstance());
+		JTextField cropType = new JTextField();
+		
+		name.setText(this.getName());
+		locX.setText(Integer.toString(this.getLocX()));
+		locY.setText(Integer.toString(this.getLocY()));
+		length.setText(Integer.toString(this.getLength()));
+		width.setText(Integer.toString(this.getWidth()));
+		price.setText(Integer.toString(this.getPrice()));
+		mPrice.setText(Integer.toString(this.getMarketValue()));
+		cropType.setText(this.getCropType());
+		
+		Object[] cropMessage = {
+				"All of these fields are required for item creation",
+				"Name:" , name,
+				"X location:", locX,
+				"Y location:", locY,
+				"Length:", length,
+				"Width:", width,
+				"Price:", price,
+				"Market Price:", mPrice,
+				"Crop Type:", cropType
+		};
+		
+		int result = JOptionPane.showConfirmDialog(null, cropMessage, "Update "+
+				this.getName(), JOptionPane.OK_CANCEL_OPTION);
+		
+		if (result == JOptionPane.OK_OPTION) {
+			this.updateInfo(name.getText(), 
+					Integer.valueOf(price.getText()), 
+					Integer.valueOf(locX.getText()), 
+					Integer.valueOf(locY.getText()), 
+					Integer.valueOf(length.getText()), 
+					Integer.valueOf(width.getText()), 
+					cropType.getText(),
+					Integer.valueOf(mPrice.getText()));
+			Fscreen.text.append("Updated "+ this.toString() + "\n");
+		}
 	}
 
 	@Override
@@ -47,7 +101,6 @@ public class Crops extends Item{
 	public String getCropType() {
 		return cropType;
 	}
-
 
 	public void setCropType(String cropType) {
 		this.cropType = cropType;
