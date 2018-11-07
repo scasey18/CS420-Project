31c985c3d965f5ac790432a723d7e84f1e55a437
Samuel W Casey <scasey18@uab.edu>
2018-11-06 21:41:35 -0600

    Replace ItemContainer.java

--- ItemContainer.java
+++ ItemContainer.java
@@ -1,20 +1,26 @@
 package farming;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Observable;
 
-public class ItemContainer {
+import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class ItemContainer extends Observable{
 	
 	ArrayList<Item> self = new ArrayList<Item>();
 	float total = 0;
 	String name; //holds name
-	float price; //Holds price of the container
+	int price; //Holds price of the container
 	int locX; //intial x
 	int locY; //intial y
 	int length; //length value
 	int width; //width value
 	float containerPrice; //holds total price for everything in the container
 	
-	public ItemContainer(String name, Float price, int locX, int locY, int length, int width) {
+	public ItemContainer(String name, int price, int locX, int locY, int length, int width) {
 		this.name = name;
 		this.locX = locX;
 		this.locY = locY;
@@ -23,16 +29,16 @@ public class ItemContainer {
 		this.price = price;
 	}
 	
+	/**
+	 * This is used exclusively for the creation of the rootnode of the jtree
+	 * @param name
+	 */
 	public ItemContainer(String name) {
 		this.name = name;
 		this.price = 0;
 	}
-
-	public ArrayList<Item> getSelf() {
-		return self;
-	}
 	
-	public void updateInfo(String name, int locX, int locY,int length, int width, float price) {
+	public void updateInfo(String name, int locX, int locY,int length, int width, int price) {
 		this.setName(name);
 		this.setPrice(price);
 		this.setLocX(locX);
@@ -41,40 +47,41 @@ public class ItemContainer {
 		this.setWidth(width);
 	}
 	
-	/**
-	 * This will calculate the total price of all items in this container
-	 */
-	public void totalPrice() {
-		total = 0;
-		for (Item a : self) {
-			total += a.getPrice();
+	public void showInfo(){
+		JTextField name = new JTextField();
+		JTextField locX = new JFormattedTextField(NumberFormat.getNumberInstance());
+		JTextField locY = new JFormattedTextField(NumberFormat.getNumberInstance());
+		JTextField length = new JFormattedTextField(NumberFormat.getNumberInstance());
+		JTextField width = new JFormattedTextField(NumberFormat.getNumberInstance());
+		JTextField price = new JFormattedTextField(NumberFormat.getNumberInstance());
+		
+		name.setText(this.getName());
+		locX.setText(Integer.toString(this.getLocX()));
+		locY.setText(Integer.toString(this.getLocY()));
+		length.setText(Integer.toString(this.getLength()));
+		width.setText(Integer.toString(this.getWidth()));
+		price.setText(Integer.toString(this.getPrice()));
+		
+		Object[] Message = {
+				"All of these fields are required for item creation",
+				"Name:" , name,
+				"X location:", locX, "Y location:", locY,
+				"Length:", length, "Width:", width,
+				"Price:", price
+		};
+		
+		int result = JOptionPane.showConfirmDialog(null, Message, "Update "+
+				this.getName(), JOptionPane.OK_CANCEL_OPTION);
+		
+		if (result == JOptionPane.OK_OPTION) {
+			this.updateInfo(name.getText(), 
+					Integer.valueOf(locX.getText()), 
+					Integer.valueOf(locY.getText()), 
+					Integer.valueOf(length.getText()), 
+					Integer.valueOf(width.getText()), 
+					Integer.valueOf(price.getText()));
+			Fscreen.text.append("Updated "+ this.toString() + "\n");
 		}
-		containerPrice = total + this.price;
-	}
-	
-	/**
-	 * This adds a item to the container but does not if the same item already exists
-	 * @param a, item you wish to add to the container
-	 */
-	public void addItem(Item a) {
-		if (!self.contains(a)) {
-			self.add(a);
-			totalPrice();
-		}
-	}
-	
-	public void removeItem(Item a) {
-		if (self.contains(a)) {
-			self.remove(a);
-			totalPrice();
-		}
-	}
-	
-	/**
-	 * This method will remove all items from this container
-	 */
-	public void removeAllItems() {
-		self.clear();
 	}
 
 	public String getName() {
@@ -85,11 +92,11 @@ public class ItemContainer {
 		this.name = name;
 	}
 
-	public float getPrice() {
+	public int getPrice() {
 		return price;
 	}
 
-	public void setPrice(float price) {
+	public void setPrice(int price) {
 		this.price = price;
 	}
 
